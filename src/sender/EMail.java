d22a57dc0bcebd2bea91421c015c4d8d23547296
Fabian <fabian.wib@gmail.com>
2012-10-12 16:59:17 +0200

    email

--- src/sender/EMail.java
+++ src/sender/EMail.java
@@ -10,24 +10,30 @@ public class EMail {
 	public static void main(String[] args) {
 
 		// Recipient's email ID needs to be mentioned.
-		String to = "berthold.wiblishauser@haw-hamburg.de";
+//		String to = "berthold.wiblishauser@haw-hamburg.de";
+		String to = "zappper@trash-mail.com";
 
 		// Sender's email ID needs to be mentioned
 		String from = "Donald.Duck@Disneyland.de";
 
 		// Assuming you are sending email from localhost
-		String host = "localhost";
+//		String host = "localhost";
+		String host = "haw-mailer.haw-hamburg.de";
 
 		// Get system properties
 		Properties properties = System.getProperties();
 
 		// Setup mail server
-		properties.setProperty("haw-mailer.haw-hamburg.de", host);
-
+		properties.setProperty("a", host);
+		try {
 		// Get the default Session object.
 		Session session = Session.getDefaultInstance(properties);
+		
+		String user = "abb664";
+		String pw = "xxx";
 
-		try {
+		Transport transport = session.getTransport("smtp");
+			
 			// Create a default MimeMessage object.
 			MimeMessage message = new MimeMessage(session);
 
@@ -45,7 +51,10 @@ public class EMail {
 			message.setText("This is actual message");
 
 			// Send message
-			Transport.send(message);
+			transport.connect(host,995,user,pw);
+		    transport.sendMessage(message,message.getAllRecipients());
+		    transport.close();
+//			Transport.send(message);
 			System.out.println("Sent message successfully....");
 		} catch (MessagingException mex) {
 			mex.printStackTrace();
